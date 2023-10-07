4b528f77173805baff6a72edf3c5d8fe4beedafe
Sacchi4649 <sanyadika@gmail.com>
2023-10-08 01:39:48 +0700

    menambahkan user route pada index

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@
 const router = require("express").Router();
 const MahasiswaRoute = require("./mahasiswaRoute");
 const DosenRoute = require("./dosenRoute");
+const userRoute = require("./userRoute");
 
 router.use("/mahasiswa", MahasiswaRoute);
 router.use("/dosen", DosenRoute);
+router.use("/", userRoute);
 
 module.exports = router;
