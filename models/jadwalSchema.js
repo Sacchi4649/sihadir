a86e36557bba5e563bcc59bc8e8065b618f7e0a0
Sacchi4649 <sanyadika@gmail.com>
2023-10-15 22:49:51 +0700

    membuat skema untuk jadwal

--- models/jadwalSchema.js
+++ models/jadwalSchema.js
@@ -1,6 +1,42 @@
 const mongoose = require("mongoose");
 
-const jadwalSchema = new mongoose.Schema({});
+const jadwalSchema = new mongoose.Schema({
+  hari: {
+    type: String,
+    enum: ["senin", "selasa", "rabu", "kamis", "jumat"],
+    required: true,
+  },
+  jam_mulai: {
+    type: String,
+    required: true,
+  },
+  jam_selesai: {
+    type: String,
+    required: true,
+  },
+  ruang: {
+    type: String,
+    required: true,
+  },
+  kelas: {
+    type: String,
+    required: true,
+  },
+  tahun: {
+    type: String,
+    required: true,
+  },
+  dosen: {
+    id: { type: String, required: true },
+    nama: { type: String, required: true },
+    nip: { type: String, required: true },
+  },
+  matakuliah: {
+    id: { type: String, required: true },
+    nama: { type: String, required: true },
+    semester: { type: String, required: true },
+  },
+});
 
 const Jadwal = mongoose.model("Jadwal", jadwalSchema);
 module.exports = Jadwal;
