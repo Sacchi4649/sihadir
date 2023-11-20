26dc88b9572b4565ed0cbc4ff063b38130966f8d
Sacchi4649 <sanyadika@gmail.com>
2023-11-21 05:44:11 +0700

    update authorization, membuat API delete mahasiswa dan delete dosen, update routes

--- routes/matkulRoute.js
+++ routes/matkulRoute.js
@@ -5,13 +5,13 @@ const MatkulController = require("../controllers/matkulController");
 const authentication = require("../middlewares/authentication");
 const authorizationAdmin = require("../middlewares/authorizationAdmin");
 
+router.get("/", authentication, MatkulController.getAllMatkul);
 router.post(
   "/",
   authentication,
   authorizationAdmin,
   MatkulController.addMatkul
 );
-router.get("/", authentication, MatkulController.getAllMatkul);
 router.put(
   "/:id",
   authentication,
